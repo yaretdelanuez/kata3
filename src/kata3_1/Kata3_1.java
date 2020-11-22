93b214134c4f6482fb6dea7df401d9de4aad37fa
Yar <Yar@DESKTOP-Yaret>
2020-11-22 18:16:51 +0000

    kata 3 versión 1

---
+++ src/kata3_1/Kata3_1.java
@@ -0,0 +1,13 @@
+
+package kata3_1;
+
+public class Kata3_1 {
+
+
+    public static void main(String[] args) {
+        HistogramDisplay histo = new HistogramDisplay();
+        histo.execute();
+        
+    }
+    
+}
